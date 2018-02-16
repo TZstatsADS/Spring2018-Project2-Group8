dbd8f6e0cf01da28445f9bc8fd274ffe8f3329fa
xxguo28 <35466417+xxguo28@users.noreply.github.com>
2018-02-16 12:41:29 -0500

    Add files via upload

---
+++ doc/ui.R
@@ -0,0 +1,47 @@
+packages.used=c("shiny", "plotly", "shinydashboard", "leaflet")
+
+# check packages that need to be installed.
+packages.needed=setdiff(packages.used, 
+                        intersect(installed.packages()[,1], 
+                                  packages.used))
+# install additional packages
+if(length(packages.needed)>0){
+  install.packages(packages.needed, dependencies = TRUE)
+}
+
+
+library(shiny)
+library(plotly)
+library(leaflet)
+library(shinydashboard)
+
+  dashboardPage(
+    dashboardHeader(title = "Hospitals For You"),
+    skin = "green",
+    dashboardSidebar(
+      selectInput("state", label = "State", 
+                choices = c("None","AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN",
+                            "IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV",
+                            "NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN",
+                            "TX","UT","VT","VA","WA","WV","WI","WY"), selected = "None")
+    ),
+    dashboardBody(
+      fluidRow(
+        tabBox(width=12,
+               tabPanel(title="Map",width = 12,solidHeader = T,leafletOutput("map"))
+              ),
+        tabBox(width = 12,
+           
+           tabPanel('Ranking',
+                    dataTableOutput("tablerank"),
+                    tags$style(type="text/css", '#myTable tfoot {display:none;}')),
+           tabPanel('Personalized Ranking',
+                    dataTableOutput("tablerank2"),
+                    tags$style(type="text/css", '#myTable tfoot {display:none;}')
+                    ))
+    )
+        
+      )
+    )
+    
+  
