74ea14945268ae5fbe4a08f94378b91445f55cef
lanceyjt <503479097@qq.com>
2018-02-19 17:28:22 -0500

    modify icon info

--- app/server.R
+++ app/server.R
@@ -80,48 +80,48 @@ shinyServer(function(input, output){
   
   v1<-reactive({
     if (state() == "Select") {v1<-hos} 
-      else {
-        selectstate<-state()
-        v1<- hos %>% filter(State == state())}})  
+    else {
+      selectstate<-state()
+      v1<- hos %>% filter(State == state())}})  
   
   v2 <- reactive({
     if (type() == "Select") {v2 <- v1()}
-      else{
-        selecttype <- type()
-        v2 <- v1() %>% filter(Hospital.Type == type())}})
+    else{
+      selecttype <- type()
+      v2 <- v1() %>% filter(Hospital.Type == type())}})
   
   care.origin <- reactive(care.origin <- c(care1(),care2(),care3(),
                                            care4(),care5(),care6(),care7()))
+  # Dataset for the selected state
+  data.state <- reactive(data.state <- v2())
+  
+  # Care vector for 7 criterion
+  care.vec <- reactive(as.numeric(care.origin()))
+  
+  # Scores of hospitals in the selected state
+  score <- reactive(apply(data.frame(data.state()),1,calScore,care.vec = care.vec()))
+  
+  # orders for hospitals
+  ord <- reactive(order(score(),decreasing = TRUE))
+  
   
   # Outputs
-    
+  
   output$tableinfo = renderDataTable(
-      {
-        data1 <- v2()
-        infotable <- data1[, c(2, 3, 4, 9, 13)]
-        infotable$Hospital.overall.rating <- apply(data.frame(as.numeric(data1$Hospital.overall.rating)),
-                                                          1,orswitch)
-        colnames(infotable) <- c("Hospital Name","Address","City","Type",
-                             "Overall Rating")
-        infotable
+    {
+      data1 <- v2()
+      infotable <- data1[, c(2, 3, 4, 9, 13)]
+      infotable$Hospital.overall.rating <- apply(data.frame(as.numeric(data1$Hospital.overall.rating)),
+                                                 1,orswitch)
+      colnames(infotable) <- c("Hospital Name","Address","City","Type",
+                               "Overall Rating")
+      infotable
     },options = list(orderClasses = TRUE, iDisplayLength = 5, lengthMenu = c(5, 10, 15, 20)))
   
   
   output$tablerank = renderDataTable({
-    # Dataset for the selected state
-    data.state <- v2()
-    
-    # Care vector for 7 criterion
-    care.vec <- as.numeric(care.origin())
-    
-    # Scores of hospitals in the selected state
-    score <- apply(data.frame(data.state),1,calScore,care.vec = care.vec)
-    
-    # orders for hospitals
-    ord <- order(score,decreasing = TRUE)
-    
-    rank <- 1:nrow(data.state)
-    rankedtable <- cbind(rank,data.state[ord,c(2,3,4,5,6,8,9,29)])
+    rank <- 1:nrow(data.state())
+    rankedtable <- cbind(rank,data.state()[ord(),c(2,3,4,5,6,8,9,29)])
     rankedtable$payment <- apply(data.frame(rankedtable$payment),1,payswitch)
     colnames(rankedtable) <- c("Rank","Hospital Name","Address","City",
                                "State","ZIP","TEL","Type","Cost")
@@ -129,29 +129,30 @@ shinyServer(function(input, output){
     rankedtable
   },options = list(orderClasses = TRUE, iDisplayLength = 5, lengthMenu = c(5, 10, 15, 20)))
   
-
+  
   hospIcons <- iconList(emergency = makeIcon("emergency_icon.png", iconWidth = 25, iconHeight =30),
                         critical = makeIcon("critical_icon.png", iconWidth = 25, iconHeight =30),
                         children = makeIcon("children_icon.png", iconWidth = 20, iconHeight =30))
   
-      
-    
-  output$map <- renderLeaflet({
-    content <- paste(sep = "<br/>",
-                     paste("<font size=1.8>","<font color=green>","<b>",v2()$Hospital.Name,"</b>"),
-                     paste("<font size=1>","<font color=black>",v2()$Address),
-                     paste(v2()$City, v2()$State, v2()$ZIP.Code, sep = " "),  
-                     paste("(",substr(v2()[ ,"Phone.Number"],1,3),") ",substr(v2()[ ,"Phone.Number"],4,6),"-",substr(v2()[ ,"Phone.Number"],7,10),sep = ""), 
-                     paste("<b>","Hospital Type: ","</b>",as.character(v2()$Hospital.Type)),  
-                     paste("<b>","Provides Emergency Services: ","</b>",as.character(v2()[ ,"Emergency.Services"])),
-                     paste("<b>","Overall Rating: ","</b>", as.character(v2()[ ,"Hospital.overall.rating"])))
-   
-    
-   mapStates = map("state", fill = TRUE, plot = FALSE)
-   leaflet(data = mapStates) %>% addTiles() %>%
-     addPolygons(fillColor = topo.colors(10, alpha = NULL), stroke = FALSE) %>%
-     addMarkers(v2()$lon, v2()$lat, popup = content, icon = hospIcons[v2()$TF], clusterOptions = markerClusterOptions())
-  })
- }
-)
   
+  
+  output$map <- renderLeaflet({
+    v3 <- reactive(cbind(v2(),ord()))
+    content <- paste(sep = "<br/>",
+                     paste("<font size=1.8>","<font color=green>","<b>",v3()$Hospital.Name,"</b>"),
+                     paste("<font size=1>","<font color=black>",v3()$Address),
+                     paste(v3()$City, v3()$State, v3()$ZIP.Code, sep = " "),  
+                     paste("(",substr(v3()[ ,"Phone.Number"],1,3),") ",substr(v3()[ ,"Phone.Number"],4,6),"-",substr(v3()[ ,"Phone.Number"],7,10),sep = ""), 
+                     paste("<b>","Hospital Type: ","</b>",as.character(v3()$Hospital.Type)),  
+                     paste("<b>","Provides Emergency Services: ","</b>",as.character(v3()[ ,"Emergency.Services"])),
+                     paste("<b>","Medicare Overall Rating: ","</b>", as.character(v3()[ ,"Hospital.overall.rating"])),
+                     paste("<b>","Personalized Ranking: ","</b>",v3()$ord))
+    
+    
+    mapStates = map("state", fill = TRUE, plot = FALSE)
+    leaflet(data = mapStates) %>% addTiles() %>%
+      addPolygons(fillColor = topo.colors(10, alpha = NULL), stroke = FALSE) %>%
+      addMarkers(v2()$lon, v2()$lat, popup = content, icon = hospIcons[v2()$TF], clusterOptions = markerClusterOptions())
+  })
+}
+)
